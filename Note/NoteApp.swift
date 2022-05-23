6bcc15051478b43f15deed2c03fb63e558a7b2dd
Sullivan677 <decarli.sulli@gmail.com>
2022-05-23 16:06:09 +0200

    Initial Commit

---
+++ Note/NoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteApp.swift
+//  Note
+//
+//  Created by Sullivan De carli on 23/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct NoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
