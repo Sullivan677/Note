39e976eaaa9d787f3fcadb4b580520f6ce1a6e0e
Sullivan677 <decarli.sulli@gmail.com>
2022-06-10 10:39:07 +0200

    Changing AppDelegate as suggested in Firebase startup and removing Google Key

--- Note/NoteApp.swift
+++ Note/NoteApp.swift
@@ -6,18 +6,24 @@
 //
 
 import SwiftUI
-import Firebase
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
-struct NoteApp: App {
-    
-    init() {
-        FirebaseApp.configure()
-        }
+struct YourApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     var body: some Scene {
         WindowGroup {
-            ContentView()
+                ContentView()
         }
     }
 }
